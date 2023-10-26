7b74a30830c43a03a4df25fead19fc1ae7b991dc
blakecoppens <blakecoppens004@gmail.com>
2023-10-26 18:07:32 -0400

    first push

--- Main.py
+++ Main.py
@@ -1,1 +1,8 @@
-print('hello')
+
+def main(): # This is the main loop that will be repeated, add an if statement to edit it here
+    print("Choose your option:\n"
+          "1. Encoder\n"
+          "2. Decoder")
+
+
+main()
