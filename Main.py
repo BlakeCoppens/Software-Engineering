8b297fea20614f943ce8640fbd6a262479254f4c
UrBoiEvan <evan145447@gmail.com>
2023-10-30 22:02:25 -0400

    Here is the completed program with decoder

--- Main.py
+++ Main.py
@@ -22,15 +22,22 @@ def encodeFunction(): # Blake Coppens' Encoder Function
     return convertedPass
 
 
-##def decodeFunction():
-    #Place your code here
+def decodeFunction(password):  #Evan Harden's awesome decoder
+    decoded_list = []
+    decoded_pw = ''
+    for i in password:
+        decoded_list.append(str(int(i) + 7))
+    for i in decoded_list:
+        decoded_pw += i[-1]
+    return decoded_pw
 
 def main(): # This is the main loop that will be repeated, add an if statement to edit it here
     UserInput = int(input("Choose your option:\n1. Encoder\n2. Decoder\n"))
     if UserInput == 1:
         encodeFunction()
     elif UserInput == 2:
-        decodeFunction()
+        password = input("Enter password to be decoded: ")
+        print(f'Your password has been decoded: {decodeFunction(password)}')
     else:
         print("Invalid Selection.")
         
